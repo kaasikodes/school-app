b99acdb5e01a5349f67cb7fcb837628375a908e8
kaasikodes <thisbroisfresh@gmail.com>
2023-03-10 22:54:20 +0100

    clearing config n route

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -3,6 +3,12 @@ echo "Running composer"
 composer global require hirak/prestissimo
 composer install --no-dev --working-dir=/var/www/html
 
+echo "Clear config..."
+php artisan config:clear
+
+echo "Clear routes..."
+php artisan route:clear
+
 echo "Caching config..."
 php artisan config:cache
 
